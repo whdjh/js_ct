f73950947463f54074cff3605c659f4afa932f99
whdjh <99352927+whdjh@users.noreply.github.com>
2024-01-30 23:46:54 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,12 @@
+function solution(num) {
+    let res = 0;
+    for (let i = 0; i < 500; i++) {
+        if(num !== 1) {
+            num = num % 2 == 0 ? num / 2 : num * 3 + 1;
+        } 
+        else {
+            return res = i;
+        }
+    }
+    return -1;
+}
