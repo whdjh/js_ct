faec30394cba20c84450ba1381b29f9929641808
whdjh <99352927+whdjh@users.noreply.github.com>
2024-02-07 20:36:25 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,10 @@
+function solution(a, b) {
+    const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    const lastmonthday = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; 
+    let day = b + 4;
+    
+    for(let i = 0; i < a - 1; ++i) {
+        day += lastmonthday[i];
+    }
+    return week[day % 7];
+}
