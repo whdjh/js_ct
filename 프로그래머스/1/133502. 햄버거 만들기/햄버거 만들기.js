a544defa5b3080bd0ef7b89f072b8f01c9dffd57
whdjh <99352927+whdjh@users.noreply.github.com>
2024-03-11 16:24:27 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,19 @@
+function solution(ingredient) {
+    let res = 0;
+    const stack = [];
+    
+    for (let i = 0; i < ingredient.length; i++) {
+        stack.push(ingredient[i]);
+        
+        if (stack.length >= 4) {
+            const burger_ingredient = stack.slice(-4).join("");
+            
+            if (burger_ingredient === '1231') {
+                stack.splice(-4);
+                res += 1;
+            }
+        }
+    }
+    
+    return res;
+}
