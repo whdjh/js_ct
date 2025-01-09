33c9eac05a6a654f6c8383bc9b63b56db3b657dc
whdjh <99352927+whdjh@users.noreply.github.com>
2025-01-10 01:29:11 +0900

    [level 1] Title: 소수 만들기, Time: 18.64 ms, Memory: 35.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,20 @@
+function isPrime(num) {
+  for (let i = 2; i < num; i++) if (num % i === 0) return false;
+    
+  return true;
+}
+
+function solution(nums) {
+    let answer = 0;
+    let sum = 0;
+    
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i + 1; j < nums.length; j++) {
+            for(let k = j + 1; k < nums.length; k++) {
+                sum = nums[i] + nums[j] + nums[k];
+                if(isPrime(sum)) answer += 1;
+            }
+        }
+    }
+    return answer;
+}
