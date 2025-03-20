c7f7cd4b56c82791d0bc2187198e49b19c8b6cb9
whdjh <99352927+whdjh@users.noreply.github.com>
2025-03-20 19:13:49 +0900

    [level 2] Title: 귤 고르기, Time: 623.11 ms, Memory: 193 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -2,13 +2,12 @@ function solution(k, tangerine) {
     let answer = 0;
     const max = Math.max(...tangerine);
     let arr = new Array(max + 1).fill(0);
+    let current = k;
+    let index = 0;
     
     for(let i = 0; i < tangerine.length; i++) arr[tangerine[i]]++;
     
     arr.sort((a, b) => b - a);
-    
-    let current = k;
-    let index = 0;
     
     while(current > 0) {
         current -= arr[index];
