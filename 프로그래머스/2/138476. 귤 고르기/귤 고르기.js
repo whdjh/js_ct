bcd7439904c2051dacbf7f58d9c63364f79fdad8
whdjh <99352927+whdjh@users.noreply.github.com>
2025-03-20 19:12:44 +0900

    [level 2] Title: 귤 고르기, Time: 796.23 ms, Memory: 193 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,19 @@
+function solution(k, tangerine) {
+    let answer = 0;
+    const max = Math.max(...tangerine);
+    let arr = new Array(max + 1).fill(0);
+    
+    for(let i = 0; i < tangerine.length; i++) arr[tangerine[i]]++;
+    
+    arr.sort((a, b) => b - a);
+    
+    let current = k;
+    let index = 0;
+    
+    while(current > 0) {
+        current -= arr[index];
+        index++;
+    }
+    
+    return index;
+}
